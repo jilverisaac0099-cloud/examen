104a863403dacc730b6cddec27c25e684067e5e3
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-06-25 20:35:43 -0600

    feat(costumer-request)

--- app/Http/Requests/CostumerRequest.php
+++ app/Http/Requests/CostumerRequest.php
@@ -7,44 +7,59 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CostumerRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|unique',
-            'telephone' => 'integer|required|unique',
-            'balance' => 'required|decimal',
-            'credit_limit' => 'decimal|required',
-            'discount' => 'string|requerid|',
-            'registration_date' => 'date|requerid',
-            'customer_status' =>'string|requerid|',
-
+            'name' => 'string|required|min:3|max:255',
+            'email' => 'string|required|email|max:255',
+            'telephone' => 'string|required|min:7|max:255',
+            'balance' => 'numeric|required',
+            'credit_limit' => 'numeric|required',
+            'discount' => 'numeric|required',
+            'registration_date' => 'date|required',
+            'customer_status' => 'string|required|min:3|max:20',
         ];
     }
+
     public function messages(): array
+    {
+        return [
+            'name.string' => 'El nombre solo permite caracteres',
+            'name.required' => 'El nombre es requerido',
+            'name.min' => 'El mínimo de caracteres es 3',
+            'name.max' => 'El maximo de caracteres es 255',
+
+            'email.string' => 'El correo electronico solo permite caracteres',
+            'email.required' => 'El correo electronico es requerido',
+            'email' => 'Debe ser un correo electronico válido',
+            'email.max' => 'El máximo de caracteres es 255',
+
+            'telephone.string' => 'El teléfono solo permite caracteres',
+            'telephone.required' => 'El telefono es requerido',
+            'telephone.min' => 'El mínimo de caracteres es 7',
+            'telephone.max' => 'El máximo de caracteres es 255',
+
+            'balance.numeric' => 'El saldo solo permite números',
+            'balance.required' => 'El saldo es requerido',
+
+            'credit_limit.numeric' => 'El límite de crédito solo permite números',
+            'credi_limit' => 'El limite de credito es requerido',
+
+            'discount.numeric' => 'El descuento solo permite números',
+            'discount.required' => 'El descuento es requerido',
+
+            'registration_date.date' => 'Debe ser una fecha válida',
+            'registration_date.required' => 'La fecha de registro es requerida',
+
+            'customer_status.string' => 'El estado de cliente solo permite caracteres',
+            'customer_status.required' => 'El estado de cliente es requerido',
+            'customer_status.min' => 'El mínimo de caracteres es 3',
+            'customer_status.max' => 'El máximo de caracteres es 20',
+        ];
+    }
 }
-    return[
-        'name.requerid' => 'El nombre es requerido',
-        'name.string' => 'El nombre debe de ser una cadena de texto',
-        'name.max='> 'El nombre debe de tener menos de 255 caracteres',
-
-        'email.requerid' => 'El nombre es requerido',
-        'email.string' => 'El nombre debe de ser una cadena de texto',
-        'email.max='> 'El nombre debe de tener menos de 255 caracteres',
-
-    
-
-    ]
