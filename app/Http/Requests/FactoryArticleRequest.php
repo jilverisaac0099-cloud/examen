196605916b70f168ad074b08137226e31f15097d
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-06-25 23:37:22 -0600

    feat(request-FactoryArticle)

--- app/Http/Requests/FactoryArticleRequest.php
+++ app/Http/Requests/FactoryArticleRequest.php
@@ -18,30 +18,30 @@ class FabricaArticuloRequest extends FormRequest
             'id_factory' => 'integer|required',
             'id_article' => 'integer|required',
             'current_stock' => 'numeric|required',
-            'cost' => 'numeric|required',
-            'tiempo_estimado_entrega' => 'string|required|min:3|max:50',
+            'negotiated_cost' => 'numeric|required',
+            '' => 'string|required|min:3|max:50',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'id_fabrica.integer' => 'El id de fábrica solo permite números enteros',
-            'id_fabrica.required' => 'El campo es requerido',
+            'id_factory.integer' => 'El id de fábrica solo permite números enteros',
+            'id_factory.required' => 'El id de la fabrica es requerido',
 
-            'id_articulo.integer' => 'El id de artículo solo permite números enteros',
-            'id_articulo.required' => 'El campo es requerido',
+            'id_article.integer' => 'El id de artículo solo permite números enteros',
+            'id_article.required' => 'El id del articulo es requerido',
 
-            'existencias_actuales.numeric' => 'Las existencias solo permiten números',
-            'existencias_actuales.required' => 'El campo es requerido',
+            'current_stock' => 'Las existencias solo permiten números',
+            'current_stock.required' => 'Las existencias es requerido',
 
-            'costo_negociado_prov.numeric' => 'El costo solo permite números',
-            'costo_negociado_prov.required' => 'El campo es requerido',
+            'negotiated_cost.numeric' => 'El costo solo permite números',
+            'negotiated_cost.required' => 'El costo es requerido',
 
-            'tiempo_estimado_entrega.string' => 'El tiempo estimado solo permite caracteres',
-            'tiempo_estimado_entrega.required' => 'El campo es requerido',
-            'tiempo_estimado_entrega.min' => 'El mínimo de caracteres es 3',
-            'tiempo_estimado_entrega.max' => 'El máximo de caracteres es 50',
+            'delivery_time.string' => 'El tiempo de entrega solo permite caracteres',
+            'delivery_time.required' => 'El tiempo de entrega es requerido',
+            'delivery_time.min' => 'El mínimo de caracteres es 3',
+            'delivery_time.max' => 'El máximo de caracteres es 50',
         ];
     }
 }
