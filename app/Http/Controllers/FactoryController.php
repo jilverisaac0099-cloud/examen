fbc297a2bcd884f35ebec33d59d34eb660388aff
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-07-03 20:45:27 -0600

    feat: agregar FactoryController

---
+++ app/Http/Controllers/FactoryController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Factory;
+use App\Http\Requests\FactoryRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class FactoryController
+{
+    public function index(): View
+    {
+        $factories = Factory::all();
+
+        return view('factories.index', compact('factories'));
+    }
+
+    public function create(): View
+    {
+        return view('factories.create');
+    }
+
+    public function store(FactoryRequest $request): RedirectResponse
+    {
+        Factory::create($request->validated());
+
+        return redirect()->route('factories.index')
+            ->with('success', 'Fabrica creada correctamente.');
+    }
+
+    public function show(Factory $factory): View
+    {
+        $factory = Factory::with('articles')->findOrFail($factory->id);
+
+        return view('factories.show', compact('factory'));
+    }
+
+    public function edit(string $id): View
+    {
+        $factory = Factory::findOrFail($id);
+
+        return view('factories.edit', compact('factory'));
+    }
+
+    public function update(FactoryRequest $request, string $id): RedirectResponse
+    {
+        $factory = Factory::findOrFail($id);
+        $factory->update($request->validated());
+
+        return redirect()->route('factories.index')
+            ->with('success', 'Fabrica actualizada correctamente.');
+    }
+
+    public function destroy(string $id): RedirectResponse
+    {
+        $factory = Factory::findOrFail($id);
+        $factory->delete();
+
+        return redirect()->route('factories.index')
+            ->with('success', 'Fabrica eliminada correctamente.');
+    }
+}
